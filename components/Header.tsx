ac6ae5268e5d9cd1ec4b93691269d8d51d767326
Moses Julian <mosesjul1425@gmail.com>
2025-11-28 17:09:42 +1100

    fix: added color configs. actually read the docs now

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header>
-            <h1>Welcome to Summerforest</h1>
+            <h1 className="text-primary">Welcome to Summerforest</h1>
         </header>
     );
 }
