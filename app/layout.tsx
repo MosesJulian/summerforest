0ca32a23dd53d609b00d405acbfb8ce8ae7a5dc1
Moses Julian <mosesjul1425@gmail.com>
2025-11-28 16:23:06 +1100

    feat: new fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu, Ubuntu_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ubuntu = Ubuntu({
+  weight: ["400", "700"],
   subsets: ["latin"],
+  variable: "--font-ubuntu",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ubuntuMono = Ubuntu_Mono({
+  weight: ["400", "700"],
   subsets: ["latin"],
+  variable: "--font-ubuntu-mono",
+
 });
 
 export const metadata: Metadata = {
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ubuntu.variable} ${ubuntuMono.variable} antialiased`}
       >
         {children}
       </body>
