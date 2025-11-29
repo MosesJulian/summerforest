0f797c9df01cc443eedd503a68f91bae89b8e2a2
Moses Julian <mosesjul1425@gmail.com>
2025-11-29 15:12:41 +1100

    feat: added an introduction or startup screen

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,8 +1,89 @@
+"use client";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+interface CarouselItem {
+    imageUrl: string;
+    altText: string;
+    caption: string;
+}
+
+const CarouselItems: CarouselItem[] = [
+    {
+        imageUrl: '/start1.jpeg',
+        altText: 'By the pool side',
+        caption: 'Relax and Enjoy the Pool View',
+    },
+    {
+        imageUrl: '/start2.jpeg',
+        altText: 'Breakfast time',
+        caption: 'Enjoy your Delicious Morning Meal',
+    },
+    {
+        imageUrl: '/start3.jpeg',
+        altText: 'Cappuccino by the pool',
+        caption: 'Drink your Coffee by the Pool',
+    },
+    {
+        imageUrl: '/start4.jpeg',
+        altText: 'Banana yogurt desert',
+        caption: 'Eat a Tasty Banana Yogurt Dessert',
+    },
+]
+
+const SlideDuration = 3000;
+const TransitionDuration = 500;
+
 const Body = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % CarouselItems.length);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(nextSlide, SlideDuration);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <section>
-            <p>This is the body of the Summerforest application.</p>
-        </section>
+        <div className="w-full h-full flex flex-col items-center justify-center space-y-8">
+            <section>
+                <div className="relative w-screen h-screen overflow-hidden">
+                    <div
+                    className='flex h-full transition-transform ease-in-out'
+                    style={{ transform: `translateX(-${currentIndex * 100}%)`, transitionDuration: `${TransitionDuration}ms` }}
+                    >
+                        {CarouselItems.map((item, index) => (
+                            <div key={index} className='w-full h-full relative flex-none bg-linear-b bg-background/30'>
+                                <Image
+                                    src={item.imageUrl}
+                                    alt={item.altText}
+                                    fill 
+                                    objectFit='cover'
+                                />
+                                <div 
+                                    className="absolute inset-0 z-0 bg-linear-b bg-primary/30"
+                                />
+                                <div className="absolute inset-0 flex flex-col items-center justify-center p-8 z-10">
+                                    <p className="text-text-secondary text-6xl font-extrabold tracking-tight p-4 text-center">
+                                    {item.caption}
+                                    </p>
+                                    <Link
+                                        href={"https://www.booking.com/hotel/id/summerforest-watukarung.html"}
+                                        target="_blank"
+                                        className="mt-6 px-8 py-3 bg-primary text-text-secondary font-medium rounded-lg shadow-lg hover:bg-secondary transition duration-300 transform hover:scale-105"    
+                                    >
+                                        Book Your Stay Now
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 };
 
