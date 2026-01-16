beee7a27e919e4973693826ebb77d8fcd3bc17b5
Moses Julian <mosesjul1425@gmail.com>
2026-01-16 14:46:54 +1100

    feat: added contacts section

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,4 +1,5 @@
 import About from "./About";
+import Contacts from "./Contacts";
 import Hero from "./Hero";
 
 const Body = () => {
@@ -6,6 +7,7 @@ const Body = () => {
         <div className="flex flex-col items-center justify-center">
             <Hero />
             <About />
+            <Contacts />
         </div>
     );
 };
---
+++ components/Contacts.tsx
@@ -0,0 +1,37 @@
+import { FaWhatsapp, FaInstagram, FaFacebook, FaPhone, FaEnvelope } from 'react-icons/fa6';
+
+const Contacts = () => {
+    return (
+        <div className="w-full grid grid-cols-5 gap-6 py-12 px-32 items-center justify-between">
+            <div className="flex flex-col gap-6 col-span-2">
+                <h2 className="text-4xl font-bold text-text text-center">Contact Us</h2>
+                <a href="https://wa.me/6285280010087" target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-text hover:text-secondary hover:font-bold transition duration-300">
+                    <FaWhatsapp size={28} />
+                    +62-852-8001-0087
+                </a>
+                <a href="tel:+6285280010087" target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-text hover:text-secondary transition duration-300">
+                    <FaPhone size={28} />
+                    +62-852-8001-0087
+                </a>
+                <a href="mailto:summerforest.watukarung@gmail.com" target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-text hover:text-secondary hover:font-bold transition duration-300">
+                    <FaEnvelope size={28} />
+                    summerforest.watukarung@gmail.com
+                </a>
+                <a href="https://instagram.com/summerforest_watukarung" target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-text hover:text-secondary hover:font-bold transition duration-300">
+                    <FaInstagram size={28} />
+                    summerforest_watukarung
+                </a>
+                <a href="https://www.facebook.com/p/Summerforestwatukarung-61575491987223/" target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-text hover:text-secondary hover:font-bold transition duration-300">
+                    <FaFacebook size={28} />
+                    Summerforestwatukarung
+                </a> 
+                </div>
+            <div className="flex flex-col gap-6 col-span-3">
+                <h2 className="text-2xl font-semibold text-text text-center">Visit Us</h2>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.6868614741547!2d110.97112727500969!3d-8.234209891798765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7bdd9017c40931%3A0x372a086487942d91!2sSummerforest%20Watukarung!5e0!3m2!1sen!2sau!4v1767400696281!5m2!1sen!2sau" width="600" height="450" loading="lazy" className="border-0 self-center"></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default Contacts;
