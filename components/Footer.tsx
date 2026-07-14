146f00de2089f3182341211ad557c8c8880cd6ae
Moses Julian <mosesjul1425@gmail.com>
2026-07-14 17:01:25 +1000

    changed the year of the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="items-center justify-center text-center p-12">
-            <span className="font-semibold text-xl">© 2024 Summerforest. All rights reserved.</span>
+            <span className="font-semibold text-xl">© 2026 Summerforest. All rights reserved.</span>
         </footer>
     );
 };
