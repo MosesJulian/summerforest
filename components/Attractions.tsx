97e8d557670b71a1433491440422d529f2a12e50
Moses Julian <mosesjul1425@gmail.com>
2026-08-17 16:55:45 +1000

    feat: made description of attractions hidden when screen size is too small and made the text smaller on smaller screens

--- components/Attractions.tsx
+++ components/Attractions.tsx
@@ -52,13 +52,13 @@ const Attractions = () => {
                 >
                     <FaLessThan size={30} />
                 </button>
-                <div className="relative w-full max-w-full md:max-w-[75%] lg:max-w-[50%] min-h-150 h-full">
+                <div className="relative w-full aspect-video h-full">
                     {attractionImages.map((item, index) => (
                         <div key={index} className={cn("absolute inset-0 w-full h-full transition-opacity duration-500 opacity-0", index === attractionImageIndex && "opacity-100")}>
                             <Image src={item.src} alt={item.alt} fill className={cn("object-cover w-full h-auto")} loading="lazy" sizes="(max-width: 768px) 75vw, (max-width: 1024px) 50vw, 100vw" />
-                            <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white p-4">
-                                <h3 className="text-base md:text-lg lg:text-xl font-bold">{item.alt}</h3>
-                                <p className="text-sm md:text-base">{item.description}</p>
+                            <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white p-2 md:p-4">
+                                <h3 className="text-sm sm:text-base md:text-lg lg:text-xl font-bold">{item.alt}</h3>
+                                <p className="hidden sm:block text-sm md:text-base">{item.description}</p>
                             </div>
                         </div>
                     ))}
