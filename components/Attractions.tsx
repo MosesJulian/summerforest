49ec74d0e3dbac73c922ff837376eea984ff855c
Moses Julian <mosesjul1425@gmail.com>
2026-08-17 17:07:02 +1000

    feat: changed the attraction's arrows design

--- components/Attractions.tsx
+++ components/Attractions.tsx
@@ -46,13 +46,13 @@ const Attractions = () => {
         <section className="bg-cyan-600/75 w-full h-auto items-center justify-center flex flex-col gap-4 py-8 scroll-mt-18" id="attractions">
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white text-center">Nearby Attractions</h2>
             <div className="flex w-full items-stretch justify-center p-4">
-                <button
-                    className="flex items-center bg-white/90 hover:bg-gray-800/60 transition-colors duration-300 p-0 sm:p-2 md:p-4 text-black hover:text-white rounded-l-4xl"
-                    onClick={() => handlePrev(attractionImages, setAttractionImageIndex)}
-                >
-                    <FaLessThan size={30} />
-                </button>
-                <div className="relative w-full aspect-video h-full">
+                <div className="relative w-full lg:w-[75%] xl:w-[50%] aspect-video">
+                    <button
+                        className="absolute z-10 left-0 top-1/2 -translate-y-1/2 h-full hover:bg-black/20 transition-colors duration-300 text-white px-2"
+                        onClick={() => handlePrev(attractionImages, setAttractionImageIndex)}
+                    >
+                        <FaLessThan size={30} />
+                    </button>
                     {attractionImages.map((item, index) => (
                         <div key={index} className={cn("absolute inset-0 w-full h-full transition-opacity duration-500 opacity-0", index === attractionImageIndex && "opacity-100")}>
                             <Image src={item.src} alt={item.alt} fill className={cn("object-cover w-full h-auto")} loading="lazy" sizes="(max-width: 768px) 75vw, (max-width: 1024px) 50vw, 100vw" />
@@ -62,13 +62,13 @@ const Attractions = () => {
                             </div>
                         </div>
                     ))}
+                    <button
+                        className="absolute z-10 right-0 top-1/2 -translate-y-1/2 h-full hover:bg-black/20 transition-colors duration-300 text-white px-2"
+                        onClick={() => handleNext(attractionImages, setAttractionImageIndex)}
+                    >
+                        <FaGreaterThan size={30} />
+                    </button>
                 </div>
-                <button
-                    className="flex items-center bg-white/90 hover:bg-gray-800/60 transition-colors duration-300 p-0 sm:p-2 md:p-4 text-black hover:text-white rounded-r-4xl"
-                    onClick={() => handleNext(attractionImages, setAttractionImageIndex)}
-                >
-                    <FaGreaterThan size={30} />
-                </button>
             </div>
         </section>
     );
